3b9818de4b672c0fcc46d6ef6eae8ba460d099f4
nzyorke <billyridgwayotr@gmail.com>
2022-08-05 09:12:55 +1200

    this is my second commit

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    console.log("Hello world");
-res.send('<h1>Hello world</h1>')
-})
+app.get("/", (req, res) => {
+  console.log("Hello world");
+  res.send("<h1>Hello world</h1>");
+});
 
 app.listen(port, () => {
-    console.log(`Our app is running on port${port}`)
-})
+  console.log(`Our app is running on port${port}`);
+});
+
+console.log("This is my second commit");
